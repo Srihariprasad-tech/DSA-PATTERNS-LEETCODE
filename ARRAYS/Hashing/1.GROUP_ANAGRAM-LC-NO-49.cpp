b65332a6f0519e44d7b78d27a9db652cee02e358
SRI HARI PRASAD S <srihariprasads91@gmail.com>
2026-09-02 23:30:58 +0530

    Implement groupAnagrams function in C++

---
+++ ARRAYS/Hashing/1.GROUP_ANAGRAM-LC-NO-49.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mp;
+    for(string s:strs)
+    {
+        vector<int>count(26,0);
+        for(char ch :s)
+        {
+            count[ch-'a']++;
+        }
+        string key="";
+        for(int i=0;i<26;i++)
+        {
+            key+=to_string(count[i])+"#";
+        }
+        mp[key].push_back(s);
+    }
+vector<vector<string>>ans;
+for(auto& pair:mp)
+{
+    ans.push_back(pair.second);
+}
+return ans;
+
+    }
+};
