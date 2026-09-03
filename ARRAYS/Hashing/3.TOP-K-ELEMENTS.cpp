cc6c85d11d293e67f8d2992eaa2f07bce94e5bcb
SRI HARI PRASAD S <srihariprasads91@gmail.com>
2026-09-04 00:15:04 +0530

    Create 3.TOP-K-ELEMENTS.cpp

---
+++ ARRAYS/Hashing/3.TOP-K-ELEMENTS.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int>freq;
+        for(int n:nums)
+        {
+            freq[n]++;       }
+    vector<pair<int,int>>arr;
+    for(auto x:freq)
+    {
+        arr.push_back({x.first,x.second});
+    }
+    sort(arr.begin(),arr.end(),[](auto &a, auto&b)
+    {
+        return a.second>b.second;
+    });
+    vector<int>ans;
+    for(int i=0;i<k;i++)
+    {
+        ans.push_back(arr[i].first);
+    }
+    return ans;
+    }
+};
