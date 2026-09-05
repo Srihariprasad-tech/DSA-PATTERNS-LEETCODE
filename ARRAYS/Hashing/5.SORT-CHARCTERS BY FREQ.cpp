8b176a3424a51393b85232f93482b2ae2cbb6550
SRI HARI PRASAD S <srihariprasads91@gmail.com>
2026-09-05 14:08:49 +0530

    Create 5.SORT-CHARCTERS BY FREQ.cpp

---
+++ ARRAYS/Hashing/5.SORT-CHARCTERS BY FREQ.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int>freq;
+        for(char ch:s)
+        {
+            freq[ch]++;
+        }
+ vector<pair<char,int>> arr(freq.begin(),freq.end());
+ sort(arr.begin(),arr.end(),[](auto &a,auto &b)
+ {
+    return a.second>b.second;
+ });
+ string str="";
+ for(auto &p : arr)
+ {
+    str.append(p.second,p.first);
+ }
+return str;
+    }
+};
