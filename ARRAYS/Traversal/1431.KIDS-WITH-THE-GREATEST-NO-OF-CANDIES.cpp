eb060336314b091b77f7c5f05fee2efe3f75e0e6
SRI HARI PRASAD S <srihariprasads91@gmail.com>
2026-09-19 00:04:23 +0530

    Create 1431.KIDS-WITH-THE-GREATEST-NO-OF-CANDIES.cpp

---
+++ ARRAYS/Traversal/1431.KIDS-WITH-THE-GREATEST-NO-OF-CANDIES.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int>ans;
+        int val=nums[0];
+        ans.push_back(val);
+         int sum=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            ans.push_back(sum);
+        }
+        return ans;
+    }
+};
